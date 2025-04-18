ccb1f65f000d7f54ba08548786862a1b0098e6ab
litnimax <litnimaxster@gmail.com>
2025-04-18 07:36:24 +0000

    Transcript recording fix

--- connect_crm/models/call.py
+++ connect_crm/models/call.py
@@ -199,3 +199,18 @@ class CrmCall(models.Model):
         fields = super().get_widget_fields()
         fields.append('lead')
         return fields
+
+    @api.constrains('summary')
+    def register_crm_lead_call_summary(self):
+        reload_view = False
+        register_summary = self.env['connect.settings'].sudo().get_param('register_summary')
+        if not register_summary:
+            return
+        for rec in self:
+            if rec.lead and rec.summary:
+                self.register_summary_to_rec(rec.lead, rec.summary)
+                reload_view = True
+        # Reload changed view.
+        if reload_view:
+            # Reload the view of res.partner
+            self.env['connect.settings'].connect_reload_view('crm.lead')
