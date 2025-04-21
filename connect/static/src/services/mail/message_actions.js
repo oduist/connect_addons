9197635c44aa58f2e0d817972aab637ed0f0401f
Nicolae Postica <nicolae.postica2@gmail.com>
2025-04-21 19:55:53 +0300

    18.0 twilio message (#21)
    * [connect] added message configuration  (ref ODU-201)
    
    * [connect] move voice message transcript to enterprise (ref ODU-201)
    
    * [connect_enterprise] add auto answer headers (ref ODU-195)
    
    * change support mail
    
    * [connect] fix duplicate bug (ref ODU-136)
    
    * [connect] add reply in chatter (ref ODU-206)
    
    * [connect] update chatter integration (ref ODU-206)
    
    * [connect] fix action name (ref ODU-206)
    
    * [connect] set default callerid on sync (ref ODU-217)
    
    ---------
    
    Co-authored-by: litnimax <litnimaxster@gmail.com>

---
+++ connect/static/src/services/mail/message_actions.js
@@ -0,0 +1,45 @@
+import {_t} from "@web/core/l10n/translation"
+import {registry} from "@web/core/registry"
+import { user } from "@web/core/user"
+
+export const messageActionsRegistry = registry.category("mail.message/actions")
+
+messageActionsRegistry
+    .add("sms-reply", {
+        condition: (component) =>
+            component.message.message_type !== null,
+        icon: "fa fa-mail-reply",
+        title: _t("Connect Reply"),
+        onClick: async (component) => {
+            console.log(component.message.Model.env.services)
+            const orm = component.message.Model.env.services.orm
+            const numbers = await orm.call("mail.message", 'get_message_numbers', [component.message.id])
+
+            if (!numbers) {
+                const notification = component.message.Model.env.services.notification
+                notification.add(_t("You can't replay to this message!"))
+                return
+            }
+
+            component.action.doAction(
+                {
+                    type: "ir.actions.act_window",
+                    target: "new",
+                    name: _t("Send Message"),
+                    res_model: "sms.composer",
+                    views: [[false, "form"]],
+                    context: {
+                        ...user.context,
+                        default_res_model: component.message.model,
+                        default_res_id: component.message.res_id,
+                        default_number_field_name: component.message.record_name,
+                        default_recipient_single_number_itf: numbers.from_number,
+                        default_recipient_single_number: numbers.from_number,
+                        default_outgoing_callerid: numbers.to_number,
+                        default_composition_mode: "comment",
+                    },
+                },
+            )
+        },
+        sequence: 120,
+    })
