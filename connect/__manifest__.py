c4d35e97c44890cd02369f694be7d812243402c3
Nicolae Postica <81677827+nicolaepostica@users.noreply.github.com>
2025-08-10 19:49:03 +0300

    17.0 user render refactoring (#36) (#37)
    * 17.0 user render refactoring (#36)
    * User render refactoring step 1
    
    * Elevenlabs always generate prompt fix
    
    * model friendly name group fix
    
    * User Call Flow done
    
    * User callflow cleanup fix
    
    * Connect Version 1.0.4
    
    * Ring first migrated to channel priority
    
    * sync with 17.0

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Connect",
-    "version": "1.0.3",
+    "version": "1.0.5",
     "author": "Oduist",
     "maintainer": "Oduist",
     "live_test_url": "https://connect-demo-18.oduist.com/",
