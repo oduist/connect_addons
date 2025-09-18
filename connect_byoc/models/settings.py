8ecb0b3d468cfb3c36c43fc92eafc5a02c54ade1
Nicolae Postica <nicolae.postica2@gmail.com>
2025-09-18 16:13:05 +0300

    Releasing additional modules under BSL license (ref ODU-283)

---
+++ connect_byoc/models/settings.py
@@ -0,0 +1,34 @@
+from odoo import fields, models, api, release
+from odoo.exceptions import ValidationError, UserError
+
+
+class Settings(models.Model):
+    _inherit = "connect.settings"
+
+    def get_usage_model_list(self):
+        res = super(Settings, self).get_usage_model_list()
+        res.extend(["byoc", "outgoing_rule"])
+        res.sort()
+        return res
+
+    def sync(self):
+        super().sync()
+        self.env["connect.byoc"].sync()
+
+    def get_external_call_route(self, number, callerId, status_url):
+        # External call to PSTN. Find outgoing rule.
+        rule = self.env["connect.outgoing_rule"].find_rule(number)
+        if not rule:
+            raise ValidationError("No outgoing rule found for this destination!")
+        callerId = self.env['connect.domain'].get_byoc_caller_id_number(rule.byoc, number, callerId)
+        twiml = """
+        <Response>
+            <Dial callerId="{}"><Number {} statusCallback='{}' statusCallbackEvent='initiated answered completed'>{}</Number></Dial>
+        </Response>
+        """.format(
+            callerId,
+            'byoc="{}"'.format(rule.byoc.sid) if rule.byoc else "",
+            status_url,
+            number,
+        )
+        return twiml
---
+++ connect_helpdesk/models/settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*
+
+from odoo import fields, models
+
+
+class HelpdeskSettings(models.Model):
+    _inherit = 'connect.settings'
+
