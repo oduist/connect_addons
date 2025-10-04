7109b5c652cfe37a933f8b687ad36f19d6660988
litnimax <litnimaxster@gmail.com>
2025-10-05 01:16:23 +0300

    Twilio sync done

--- connect/models/settings.py
+++ connect/models/settings.py
@@ -601,9 +601,9 @@ class Settings(models.Model):
         if api_url_check:
             raise ValidationError(api_url_check)
         try:
-            #self.env["connect.twiml"].sync()
-            #self.env["connect.domain"].sync()
-            #self.env["connect.number"].sync()
+            self.env["connect.twiml"].sync()
+            self.env["connect.domain"].sync()
+            self.env["connect.number"].sync()
             self.env["connect.outgoing_callerid"].sync()
             self.connect_notify("Sync complete.")
         except Exception as e:
