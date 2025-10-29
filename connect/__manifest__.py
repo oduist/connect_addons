259601a0f87bbaeec808d8fd8859ff86f0d0eb97
litnimax <litnimaxster@gmail.com>
2025-10-29 15:52:18 +0200

    Version 1.0.10

--- connect/__manifest__.py
+++ connect/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Connect',
-    'version': '1.0.9',
+    'version': '1.0.10',
     'author': 'Oduist',
     'maintainer': 'Oduist',
     'live_test_url': 'https://connect-demo-18.oduist.com/',
