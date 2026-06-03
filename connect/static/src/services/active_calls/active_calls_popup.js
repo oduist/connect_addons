22107fbd8a765110e8a8f198571eb0e2bf300a57
Nicolae Postica <nicolae.postica2@gmail.com>
2026-06-03 16:48:07 +0300

    [connect] show non-completed calls in active calls widget

--- connect/static/src/services/active_calls/active_calls_popup.js
+++ connect/static/src/services/active_calls/active_calls_popup.js
@@ -31,7 +31,7 @@ export class ConnectActiveCallsPopup extends Component {
     }
 
     async getCalls() {
-        const domain = [["status", "=", "in-progress"]]
+        const domain = [["status", "!=", "completed"]]
         this.state.calls = await this.orm.call("connect.call", "get_widget_calls", [domain])
         if (this.state.calls.length > 0) {
             this.setTimer(3000)
