017e18c6decc9a0868c011d3b65ca02769bdc6e2
Nicolae Postica <81677827+nicolaepostica@users.noreply.github.com>
2026-06-27 03:07:08 +0300

    [connect] fix AI completion composer patch crashing Chatter on Odoo 18 (#138)
    The composer patch inherited mail.Composer and xpath'd //FileUploader, but
    on Odoo 18 FileUploader lives in the mail.Composer.attachFiles sub-template.
    The unresolved xpath threw an Owl lifecycle error that crashed every Chatter
    composer (helpdesk, sales, partners, ...).
    
    - composer.xml: inherit mail.Composer.attachFiles instead of mail.Composer
    - composer.js: import rpc from @web/core/network/rpc (Composer has no this.rpc)
    - composer.js: write to composer.text (was textInputContent)
    
    Fixes #137

--- connect/static/src/core/common/composer.js
+++ connect/static/src/core/common/composer.js
@@ -2,17 +2,18 @@
 "use strict"
 
 import {patch} from "@web/core/utils/patch"
+import {rpc} from "@web/core/network/rpc"
 import {Composer} from "@mail/core/common/composer"
 
 
 patch(Composer.prototype, {
     async _onClickAiCompletion() {
-        const response = await this.rpc("/connect/ai_completion", {
+        const response = await rpc("/connect/ai_completion", {
             model: this.thread.model,
             res_id: this.thread.id,
         })
         if (response.status === 'ok') {
-            this.props.composer.textInputContent = response.message
+            this.props.composer.text = response.message
         } else {
             this.env.services.notification.add(response.error_message, {type: "warning",})
         }
