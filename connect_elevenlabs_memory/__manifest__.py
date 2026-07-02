9548a353c1f43358c1e61776131d10890c5b64ec
Nicolae Postica <nicolae.postica2@gmail.com>
2026-07-02 20:15:42 +0300

    [connect_elevenlabs_memory] scaffold module depending on connect_elevenlabs

---
+++ connect_elevenlabs_memory/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Connect ElevenLabs Memory",
+    "version": "1.0.0",
+    "author": "Oduist",
+    "maintainer": "Oduist",
+    "support": "support@oduist.com",
+    "license": "Other proprietary",
+    "category": "Phone",
+    "summary": "Long-term memory (Hindsight) for ElevenLabs voice agents",
+    "description": "",
+    "depends": ["connect_elevenlabs"],
+    "external_dependencies": {"python": ["requests"]},
+    "data": [],
+    "installable": True,
+}
