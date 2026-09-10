8647ac2678dc1794f3fd98c98a9cb4677eefa985
litnimax <litnimaxster@gmail.com>
2026-09-10 15:46:57 +0000

    Fix json route type deprecation for Odoo 19.0

--- connect_book/controllers/main.py
+++ connect_book/controllers/main.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from odoo import http, release
 from odoo.http import request
+
+route_type = "json" if release.version_info[0] < 19.0 else 'jsonrpc'
 
 
 class ConnectBookController(http.Controller):
     """Thin JSON wrapper over the ``connect.book`` model for the client actions."""
 
-    @http.route("/connect_book/book", type="jsonrpc", auth="user")
+    @http.route("/connect_book/book", type=route_type, auth="user")
     def book(self):
         return request.env["connect.book"].get_book()
 
-    @http.route("/connect_book/admin", type="jsonrpc", auth="user")
+    @http.route("/connect_book/admin", type=route_type, auth="user")
     def admin_book(self):
         # get_admin_book enforces the system-admin group itself.
         return request.env["connect.book"].get_admin_book()
 
-    @http.route("/connect_book/changes", type="jsonrpc", auth="user")
+    @http.route("/connect_book/changes", type=route_type, auth="user")
     def changes(self):
         return request.env["connect.book"].get_changes()
